aeafd36f58397957f85bead77ecbf72b5904e0e5
Mahdi Amirabadi <amirabadi.mahdi@gmail.com>
2022-04-14 23:53:07 +0430

    Add docker compose file for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const connectDB = require("./config/db");
 const errorHandler = require("./middlewares/errorMiddleware");
 const cookieParser = require("cookie-parser");
@@ -17,6 +18,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 app.use("/api/cats", require("./routes/categoryRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+const CLIENT_BUILD_PATH = path.join(__dirname, "..", "frontend", "build");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(CLIENT_BUILD_PATH));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(CLIENT_BUILD_PATH, "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
